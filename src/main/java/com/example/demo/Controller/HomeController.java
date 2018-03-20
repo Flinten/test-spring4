8c636a33867a5ea6957f8efacfa1ee7058e1db1d
Unknown <ckflintrup@gmail.com>
2018-03-20 10:50:24 +0100

    This is a update

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -2,11 +2,12 @@ package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Ejer on 07-03-2018.
  */
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
